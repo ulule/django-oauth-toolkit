945ab5e540ad9c3a79a6825c9d284aa54d0c03f9
Federico Dolce <psykopear@gmail.com>
2015-10-29 18:08:13 +0100

    Added possibility to specify a default list of scopes

--- oauth2_provider/forms.py
+++ oauth2_provider/forms.py
@@ -6,7 +6,7 @@ from .models import Application
 class AllowForm(forms.Form):
     allow = forms.BooleanField(required=False)
     redirect_uri = forms.CharField(widget=forms.HiddenInput())
-    scope = forms.CharField(required=False, widget=forms.HiddenInput())
+    scope = forms.CharField(widget=forms.HiddenInput())
     client_id = forms.CharField(widget=forms.HiddenInput())
     state = forms.CharField(required=False, widget=forms.HiddenInput())
     response_type = forms.CharField(widget=forms.HiddenInput())
