901556067e2396d6cb436fbda876b14283b921d1
Gilles Fabio <gilles@gillesfabio.com>
2015-11-09 17:08:22 +0100

    Better migrations.

--- oauth2_provider/migrations/0003_add_application_grant_types.py
+++ oauth2_provider/migrations/0003_add_application_grant_types.py
@@ -2,18 +2,26 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+
 import separatedvaluesfield.models
 
 
-def add_grant_types(apps, schema_editor):
+def add_application_grant_types(apps, schema_editor):
+    """
+    Adds Application.grant_types field to table.
+    """
     Application = apps.get_model('oauth2_provider', 'Application')
-    applications = Application.objects.using(schema_editor.connection.alias).all()
-    for app in applications:
+    apps = Application.objects.using(schema_editor.connection.alias).all()
+    for app in apps:
         app.grant_types = [app.authorization_grant_type]
         app.save()
 
 
 class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oauth2_provider', '0002_08_updates'),
+    ]
 
     operations = [
         migrations.AddField(
@@ -21,5 +29,5 @@ class Migration(migrations.Migration):
             name='grant_types',
             field=separatedvaluesfield.models.SeparatedValuesField(default=[b'password'], max_length=150, verbose_name='grant types', choices=[(b'authorization-code', 'Authorization code'), (b'implicit', 'Implicit'), (b'password', 'Resource owner password-based'), (b'client-credentials', 'Client credentials')]),
         ),
-        migrations.RunPython(add_grant_types),
+        migrations.RunPython(add_application_grant_types),
     ]
